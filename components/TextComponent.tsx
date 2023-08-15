3cc626045d716262a8e44362b8dde89b6d210334
Artur Piasecki <apiasecki93@gmail.com>
2023-08-15 20:24:29 +0200

    Initial commit from Create Next App THIS IS MAIN TEMPLATE FOR NEXT JS 13 WITH TAILWIND

---
+++ components/TextComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TextComponent = () => {
+  return (
+    <div className='text-red-500'>textComponent</div>
+  )
+}
+
+export default TextComponent
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,38 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
+      colors: {
+        moderateBlue: "#5457b6",
+        softRed: "#ed6468",
+        lightGrayBlue: "#c3c4ef",
+        paleRed: "#ffb8bb",
+        darkBluee: "#324152",
+        grayBluee: "#67727e",
+        lightGrayy: "#eaecf1",
+        verylightGray: "#f5f6fa",
+        whitee: "#ffffff",
+      },
+      screens: {
+        ssm: { max: "375px" },
+        mmin: { min: "376px" },
       },
     },
   },
   plugins: [],
-}
-export default config
+  wrapper: {
+    path: "src/PreviewWrapper.jsx",
+    componentName: "default",
+  },
+};
